2d4a6d6ca3fdc5bc30433c20d4aaaff1f267833d
youmiaoz <89759589+youmiaoz@users.noreply.github.com>
2022-05-16 13:40:41 +0800

    Update install.sh

--- examples/workload/wordpress/install.sh
+++ examples/workload/wordpress/install.sh
@@ -2,4 +2,4 @@ kubectl create ns wordpress
 kubectl create -f ./mysql-deployment.yaml
 sleep 5
 kubectl create -f ./wordpress-deployment.yaml
-kubectl apply -f ./ingress_v1beta.yaml
+#kubectl apply -f ./ingress_v1beta.yaml
